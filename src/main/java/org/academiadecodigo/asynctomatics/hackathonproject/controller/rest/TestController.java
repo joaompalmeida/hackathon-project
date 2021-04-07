712d303d024894a2833bb8b7b5164e404461e39f
joaompalmeida <joaompalmeida@gmail.com>
2021-04-07 17:44:24 +0100

    trial 3 for early deploy

--- src/main/java/org/academiadecodigo/asynctomatics/hackathonproject/controller/rest/TestController.java
+++ src/main/java/org/academiadecodigo/asynctomatics/hackathonproject/controller/rest/TestController.java
@@ -1,14 +1,12 @@
-package org.academiadecodigo.asynctomatics.hackathonproject.controller;
-
+package org.academiadecodigo.asynctomatics.hackathonproject.controller.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.*;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class TestController {
 
 		@RequestMapping(method = RequestMethod.GET, path = {"/", ""})
---
+++ src/main/java/org/academiadecodigo/asynctomatics/hackathonproject/controller/web/WebTestController.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.asynctomatics.hackathonproject.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class WebTestController {
+
+	@RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+	public String addCustomer(Model model) {
+		return "/test";
+	}
+
+
+}
+
